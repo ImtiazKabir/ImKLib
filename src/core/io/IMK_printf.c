545206e6b0bd0dd271e841d072df8c402d02f1b1
ImtiazKabir <imtiazkabir.imtiaz@gmail.com>
2025-01-23 18:39:09 +0600

    added thread safety for logging and printing, need to test it out

--- src/core/io/IMK_printf.c
+++ src/core/io/IMK_printf.c
@@ -1,7 +1,8 @@
+#include <pthread.h>
 #include <stdarg.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define SLUG_IMK_DIR_ROOT imklib
 #include "imklib/IMK_index_ref.slug"
@@ -11,8 +12,60 @@
 #define USING_NAMESPACE_IMK_CORE
 #include SLUG_IMK_HEADER_CORE
 
+typedef struct {
+  pthread_mutex_t lock; /* Underlying mutex lock */
+  pthread_t owner;      /* Thread ID of the owner */
+  int count;            /* Recursion count */
+} recursive_mutex_t;
+
+#define RECURSIVE_MUTEX_INITIALIZER {PTHREAD_MUTEX_INITIALIZER}
+
+static void recursive_mutex_init(recursive_mutex_t *rm) {
+  if (pthread_mutex_init(&rm->lock, NULL) != 0) {
+    perror("Failed to initialize mutex");
+    exit(EXIT_FAILURE);
+  }
+  rm->count = 0;
+}
+
+static void recursive_mutex_lock(recursive_mutex_t *rm) {
+  if (rm->count && pthread_equal(rm->owner, pthread_self())) {
+    /* If the current thread already owns the mutex, increment the count */
+    rm->count++;
+    return;
+  }
+
+  /* Otherwise, lock the underlying mutex */
+  pthread_mutex_lock(&rm->lock);
+  rm->owner = pthread_self();
+  rm->count = 1;
+}
+
+static void recursive_mutex_unlock(recursive_mutex_t *rm) {
+  if (!pthread_equal(rm->owner, pthread_self())) {
+    /* Error: Unlocking a mutex not owned by this thread */
+    fprintf(stderr, "Error: Thread does not own the mutex\n");
+    exit(EXIT_FAILURE);
+  }
+
+  rm->count--;
+  if (rm->count == 0) {
+    /* Release ownership and unlock the mutex */
+    pthread_mutex_unlock(&rm->lock);
+  }
+}
+
+static void recursive_mutex_destroy(recursive_mutex_t *rm) {
+  if (rm->count != 0) {
+    fprintf(stderr, "Error: Destroying a locked mutex\n");
+    exit(EXIT_FAILURE);
+  }
+  pthread_mutex_destroy(&rm->lock);
+}
+
 static char internal_buffer[268435456] = {0};
 static char *internal_buffer_start = internal_buffer;
+static recursive_mutex_t buf_mut = RECURSIVE_MUTEX_INITIALIZER;
 
 int PrintF(char const *fmt, ...) {
   va_list ap;
@@ -50,18 +103,24 @@ int SNPrintF(char *str, size_t size, char const *fmt, ...) {
   return ret;
 }
 
-int VPrintF(char const *fmt, va_list ap) {
-  return VFPrintF(stdout, fmt, ap);
-}
+int VPrintF(char const *fmt, va_list ap) { return VFPrintF(stdout, fmt, ap); }
 
 int VFPrintF(FILE *stream, char const *fmt, va_list ap) {
+  int ret;
+  recursive_mutex_lock(&buf_mut);
   VSNPrintF(NULL, 0, fmt, ap);
-  return fprintf(stream, "%s", internal_buffer_start);
+  ret = fprintf(stream, "%s", internal_buffer_start);
+  recursive_mutex_unlock(&buf_mut);
+  return ret;
 }
 
 int VSPrintF(char *str, char const *fmt, va_list ap) {
+  int ret;
+  recursive_mutex_lock(&buf_mut);
   VSNPrintF(NULL, 0, fmt, ap);
-  return sprintf(str, "%s", internal_buffer_start);
+  ret = sprintf(str, "%s", internal_buffer_start);
+  recursive_mutex_unlock(&buf_mut);
+  return ret;
 }
 
 static int ObjSpecifier(char *str, va_list ap) {
@@ -119,7 +178,7 @@ static int vsprintf_(char const *fmt, va_list ap) {
 
     n3 = vsprintf_(fmt + first_occur_index + strlen(first_specifier), ap);
     internal_buffer_start += n3;
-    
+
     return n1 + n2 + n3;
   } else {
     int n = vsprintf(internal_buffer_start, fmt, ap);
@@ -129,8 +188,10 @@ static int vsprintf_(char const *fmt, va_list ap) {
 }
 
 int VSNPrintF(char *str, size_t size, char const *fmt, va_list ap) {
-  char *pushed_buf_start = internal_buffer_start;
+  char *pushed_buf_start;
   int ret;
+  recursive_mutex_lock(&buf_mut);
+  pushed_buf_start = internal_buffer_start;
   ret = vsprintf_(fmt, ap);
   if (ret > 0 && str != NULL && size > 0) {
     size_t min = (size_t)ret < size - 1 ? (size_t)ret : size - 1;
@@ -138,6 +199,7 @@ int VSNPrintF(char *str, size_t size, char const *fmt, va_list ap) {
     str[min] = '\0';
   }
   internal_buffer_start = pushed_buf_start;
+
+  recursive_mutex_unlock(&buf_mut);
   return ret;
 }
-
--- src/core/logging/IMK_log.c
+++ src/core/logging/IMK_log.c
@@ -1,5 +1,7 @@
+#include <pthread.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
@@ -15,7 +17,23 @@
 #include SLUG_IMK_HEADER_ANSI_STYLE
 #include SLUG_IMK_HEADER_PRINTF
 
-static u8 log_levelmsk = LOG_MASK_ALL;
+static pthread_key_t log_levelmsk_key;
+static pthread_once_t log_levelmsk_key_once = PTHREAD_ONCE_INIT;
+static pthread_mutex_t log_mut = PTHREAD_MUTEX_INITIALIZER;
+
+static void MakeKey(void) { pthread_key_create(&log_levelmsk_key, free); }
+
+static u8 *GetLevelMask(void) {
+  u8 *msk;
+  pthread_once(&log_levelmsk_key_once, MakeKey);
+  msk = pthread_getspecific(log_levelmsk_key);
+  if (msk == NULL) {
+    msk = malloc(sizeof(*msk));
+    *msk = LOG_MASK_ALL;
+    pthread_setspecific(log_levelmsk_key, msk);
+  }
+  return msk;
+}
 
 static char const *level_strings[] = {"TRACE", "DEBUG", "INFO",
                                       "WARN",  "ERROR", "FATAL"};
@@ -30,8 +48,11 @@ static char const *level_colors[] = {
 
 void IMK_LogVFBW(u8 level, FILE *fp, char const *file, int line,
                  char const *fmt, va_list args) {
-  auto time_t rawtime = {0};
-  register struct tm const *timeinfo = NULL;
+  time_t rawtime = {0};
+  struct tm const *timeinfo = NULL;
+  u8 log_levelmsk = IMK_LogGetMask();
+
+  pthread_once(&log_levelmsk_key_once, MakeKey);
 
   if ((level > LOG_FATAL) || ((log_levelmsk & (1 << level)) == 0)) {
     return;
@@ -40,10 +61,12 @@ void IMK_LogVFBW(u8 level, FILE *fp, char const *file, int line,
   time(&rawtime);
   timeinfo = localtime(&rawtime);
 
+  pthread_mutex_lock(&log_mut);
   fprintf(fp, "%02d:%02d:%02d %-5s %s:%d: ", timeinfo->tm_hour,
           timeinfo->tm_min, timeinfo->tm_sec, level_strings[level], file, line);
   VFPrintF(fp, fmt, args);
   fprintf(fp, "\n");
+  pthread_mutex_unlock(&log_mut);
 }
 
 void IMK_LogVBW(u8 level, char const *file, int line, char const *fmt,
@@ -71,6 +94,7 @@ void IMK_LogVFCol(u8 level, FILE *fp, char const *file, int line,
                   char const *fmt, va_list args) {
   time_t rawtime = {0};
   struct tm const *timeinfo = NULL;
+  u8 log_levelmsk = IMK_LogGetMask();
 
   if ((level > LOG_FATAL) || ((log_levelmsk & (1 << level)) == 0)) {
     return;
@@ -79,12 +103,15 @@ void IMK_LogVFCol(u8 level, FILE *fp, char const *file, int line,
   time(&rawtime);
   timeinfo = localtime(&rawtime);
 
+  pthread_mutex_lock(&log_mut);
   fprintf(fp,
-          "%02d:%02d:%02d " ANSI_RESET "%s%-5s" ANSI_RESET ANSI_FG_BRIGHT_BLACK " %s:%d: " ANSI_RESET,
+          "%02d:%02d:%02d " ANSI_RESET "%s%-5s" ANSI_RESET ANSI_FG_BRIGHT_BLACK
+          " %s:%d: " ANSI_RESET,
           timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec,
           level_colors[level], level_strings[level], file, line);
   VFPrintF(fp, fmt, args);
   fprintf(fp, "\n");
+  pthread_mutex_unlock(&log_mut);
 }
 
 void IMK_LogVCol(u8 level, char const *file, int line, char const *fmt,
@@ -109,23 +136,19 @@ void IMK_LogCol(u8 level, char const *file, int line, char const *fmt, ...) {
 }
 
 void IMK_LogSetMask(u8 levelmsk) {
-  log_levelmsk = levelmsk;
-  log_levelmsk |= LOG_MASK_FATAL;
+  u8 *log_levelmsk = GetLevelMask();
+  *log_levelmsk = levelmsk;
+  *log_levelmsk |= LOG_MASK_FATAL;
 }
 
-void IMK_LogSetMin(u8 level) {
-  log_levelmsk = (u8)(((u8)-1) << level);
-  log_levelmsk |= LOG_MASK_FATAL;
-}
+void IMK_LogSetMin(u8 level) { IMK_LogSetMask((u8)(((u8)-1) << level)); }
 
-u8 IMK_LogGetMask(void) { return log_levelmsk; }
+u8 IMK_LogGetMask(void) { return *GetLevelMask(); }
 
 void IMK_LogAddMask(u8 levelmsk) {
-  log_levelmsk |= levelmsk;
-  log_levelmsk |= LOG_MASK_FATAL;
+  IMK_LogSetMask(IMK_LogGetMask() | levelmsk);
 }
 
 void IMK_LogClearMask(u8 levelmsk) {
-  log_levelmsk &= ~levelmsk;
-  log_levelmsk |= LOG_MASK_FATAL;
+  IMK_LogSetMask(IMK_LogGetMask() & ~levelmsk);
 }
