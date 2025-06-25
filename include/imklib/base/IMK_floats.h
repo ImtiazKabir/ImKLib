a77cce2222e10774067622251e3356efbc76f487
ImtiazKabir <imtiazkabir.imtiaz@gmail.com>
2025-06-25 12:14:42 +0600

    added safe float equality

--- include/imklib/base/IMK_floats.h
+++ include/imklib/base/IMK_floats.h
@@ -2,6 +2,7 @@
 #define IMK_FLOATS_H_R36EWR6MCEPR2YOZ
 
 #include <float.h>
+#include <math.h>
 
 /* Define 32-bit floating point type (f32) */
 typedef float f32;
@@ -10,6 +11,7 @@ typedef float f32;
 #define F32_EPSILON FLT_EPSILON
 #define F32_FMT "f"
 #define F32(value) ((f32)(value))
+#define F32_EQ(a, b, epsilon) (fabs((a) - (b)) < (epsilon))
 
 /* Define 64-bit floating point type (f64) */
 typedef double f64;
@@ -18,5 +20,6 @@ typedef double f64;
 #define F64_EPSILON DBL_EPSILON
 #define F64_FMT "f"
 #define F64(value) ((f64)(value))
+#define F64_EQ(a, b, epsilon) (fabs((a) - (b)) < (epsilon))
 
 #endif /* !IMK_FLOATS_H_R36EWR6MCEPR2YOZ */
